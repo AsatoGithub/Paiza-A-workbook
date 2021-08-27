bdb16406e5ac7f7cb297597e997065b4773c07e6
AsatoT <20195046@g.aut.ac.jp>
2021-08-27 23:46:19 +0900

    マップの判定・横

--- Main.java
+++ Main.java
@@ -4,10 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //  盤面の行数Ｈ,列数Ｗ,与えられる座標数Ｎ取得
+        //  盤面の行数Ｈ,列数W取得
         int H = sc.nextInt();
         int W = sc.nextInt();
-        int N = sc.nextInt();
         //Ｈ行Ｗ列の配列Array宣言
         String[][] Array = new String[H][W];
 
@@ -15,20 +14,21 @@ public class Main {
         for(int i = 0; i<W; i++){
             Array[i] = sc.next().split("");
         }
-
-        //a行ｂ列の値を#に変更
-        for(int i = 0; i<N; i++){
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                Array[a][b] = "#";
-        }
-
-        //#に置き換えた後の配列Arraiの要素を出力
+        //左端のマスの判定
         for(int i = 0; i<H; i++){
-            for(int j = 0; j<W; j++){
-                System.out.print(Array[i][j]);
+            if(Array[i][1].equals("#")){
+                System.out.println(i + " 0" );
             }
-            System.out.println("");
+            //両方の端を＃で囲まれたマスの判定
+            for(int j = 1; j<W-1;j++){
+                if(Array[i][j-1].equals("#") && Array[i][j+1].equals("#")){
+                    System.out.println(i + " " + j);
+                }
+            }
+            //右端のマスの判定
+            if(Array[i][H-2].equals("#")){
+                System.out.println(i + " " + (H-1));
+            }
         }
     }
 }
