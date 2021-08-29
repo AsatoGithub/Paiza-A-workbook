62bdd3dae7b265671e6020b5ae7ce90b17ee0fff
AsatoT <20195046@g.aut.ac.jp>
2021-08-29 14:22:39 +0900

    マップからの座標取得

--- Main.java
+++ Main.java
@@ -4,31 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //  盤面の行数Ｈ,列数W取得
-        int H = sc.nextInt();
-        int W = sc.nextInt();
-        //Ｈ行Ｗ列の配列Array宣言
+        int H = sc.nextInt(); //行数
+        int W = sc.nextInt(); //列数
+        sc.nextLine(); //行送り
         String[][] Array = new String[H][W];
 
-        //配列Arrayに座標の値格納
-        for(int i = 0; i<W; i++){
-            Array[i] = sc.next().split("");
-        }
-        //上端のマスの判定
         for(int i = 0; i<H; i++){
-            if(Array[1][i].equals("#")){
-                System.out.println(0 + " i" );
-            }
-            //上端下端を＃で囲まれたマスの判定
-            for(int j = 1; j<W-1;j++){
-                if(Array[j-1][i].equals("#") && Array[j+1][i].equals("#")){
-                    System.out.println(j + " " + i);
-                }
-            }
-            //下端のマスの判定
-            if(Array[H-2][i].equals("#")){
-                System.out.println(H-1 + " " + i);
+                String str = sc.nextLine();
+                Array[i] = str.split("");
+        }
+
+        for(int i = 0; i<H; i++){
+            for(int j = 0; j<W; j++){
+               if(Array[i][j].equals("#")){
+                    System.out.println(i + " " + j);
+               }
             }
         }
     }
-}
+    }
