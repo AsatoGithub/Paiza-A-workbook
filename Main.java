2047a7ef5706d3e8b6863af328ee93e7c8c7d390
AsatoT <20195046@g.aut.ac.jp>
2021-08-30 23:39:13 +0900

    座標系での移動・方角

--- Main.java
+++ Main.java
@@ -4,22 +4,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int H = sc.nextInt(); //行数
-        int W = sc.nextInt(); //列数
+        int Y = sc.nextInt(); //開始時点のY座標
+        int X = sc.nextInt(); //開始座標のX座標
+        int N = sc.nextInt(); //移動の回数
+        String[] direction = new String[N];
         sc.nextLine(); //行送り
-        String[][] Array = new String[H][W];
 
-        for(int i = 0; i<H; i++){
-                String str = sc.nextLine();
-                Array[i] = str.split("");
-        }
-
-        for(int i = 0; i<H; i++){
-            for(int j = 0; j<W; j++){
-               if(Array[i][j].equals("#")){
-                    System.out.println(i + " " + j);
-               }
+        for(int i = 0; i<N; i++){
+            direction[i] = sc.nextLine();
+            if(direction[i].equals("N")){
+                Y = Y - 1;
+                System.out.println(Y + " " + X);
+            }else if(direction[i].equals("S")){
+                Y = Y + 1;
+                System.out.println(Y + " " + X);
+            }else if(direction[i].equals("E")){
+                X = X + 1;
+                System.out.println(Y + " " + X);
+            }else if(direction[i].equals("W")){
+                X = X - 1;
+                System.out.println(Y + " " + X);
             }
+
         }
+
     }
-    }
+}
