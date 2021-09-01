fbcd40c0f88e683bc1b2c4d648fdae56d3fa2509
AsatoT <20195046@g.aut.ac.jp>
2021-09-01 23:45:30 +0900

    座標系での規則的な移動

--- Main.java
+++ Main.java
@@ -4,39 +4,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Y = sc.nextInt(); //開始時点のY座標
-        int X = sc.nextInt(); //開始座標のX座標
-        String D = sc.next(); //向いている方角
-        sc.nextLine(); //行送り
-        String direction = sc.nextLine(); //移動の向き
+        int X = sc.nextInt(); //開始時点のx座標
+        int Y = sc.nextInt(); //開始時点のy座標
+        int N = sc.nextInt(); //移動回数N
+        int count = 1;
+        int counter = 1;
+        int Ncloke = 0;
 
-        //向いている方角から移動したときの座標変化
-            if(D.equals("N")){
-                if(direction.equals("R")){
-                    X = X + 1;
-                }else if(direction.equals("L")){
-                    X = X - 1;
-                }
-            }else if(D.equals("E")){
-                if(direction.equals("R")){
-                    Y = Y + 1;
-                }else if(direction.equals("L")){
-                    Y = Y - 1;
-                }
-            }else if(D.equals("W")){
-                if(direction.equals("R")){
-                    Y = Y - 1;
-                }else if(direction.equals("L")){
-                    Y = Y + 1;
-                }
-            }else if(D.equals("S")){
-                if(direction.equals("R")){
-                    X = X - 1;
-                }else if(direction.equals("L")){
-                    X = X + 1;
-                }
+        while(Ncloke < N){
+            if(count == 1){
+                X = X + counter;
+                count = count + 1;
+                Ncloke = Ncloke + counter;
+            }else if(count == 2){
+                Y = Y + counter;
+                count = count + 1;
+                counter = counter + 1;
+                Ncloke = Ncloke + counter;
+            }else if(count == 3){
+                X = X - counter;
+                count = count + 1;
+                Ncloke = Ncloke + counter;
+            }else if(count == 4){
+                Y = Y - counter;
+                count = 1;
+                counter = counter + 1;
+                Ncloke = Ncloke + counter;
             }
-
-        System.out.println(Y + " " + X); //Y,X座標出力
+        }
+        System.out.println(X + " " + Y);
     }
 }
