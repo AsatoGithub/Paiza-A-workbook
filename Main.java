1aa9e5d1fb0d5a6d75a1c79f37fb3746144b30e4
AsatoT <20195046@g.aut.ac.jp>
2021-09-02 22:24:36 +0900

    マップの判定・横（修正版、ランタイムエラー）

--- Main.java
+++ Main.java
@@ -4,34 +4,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt(); //開始時点のx座標
-        int Y = sc.nextInt(); //開始時点のy座標
-        int N = sc.nextInt(); //移動回数N
-        int count = 1;
-        int counter = 1;
-        int Ncloke = 0;
+        //  盤面の行数Ｈ,列数W取得
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        //Ｈ行Ｗ列の配列Array宣言
+        String[][] Array = new String[H][W];
 
-        while(Ncloke < N){
-            if(count == 1){
-                X = X + counter;
-                count = count + 1;
-                Ncloke = Ncloke + counter;
-            }else if(count == 2){
-                Y = Y + counter;
-                count = count + 1;
-                counter = counter + 1;
-                Ncloke = Ncloke + counter;
-            }else if(count == 3){
-                X = X - counter;
-                count = count + 1;
-                Ncloke = Ncloke + counter;
-            }else if(count == 4){
-                Y = Y - counter;
-                count = 1;
-                counter = counter + 1;
-                Ncloke = Ncloke + counter;
+        //配列Arrayに座標の値格納
+        for(int i = 0; i<W; i++){
+            Array[i] = sc.next().split("");
+        }
+
+
+        for(int i = 0; i<H; i++){
+            for(int j = 0; j<W; j++){
+                //左端のマスの判定
+                if(j == 0 && Array[i][j+1].equals("#")){
+                  System.out.println(i + " 0" );
+                }
+
+                //両方の端を＃で囲まれたマスの判定
+                if(0<j && j<W-1){
+                if(Array[i][j-1].equals("#") && Array[i][j+1].equals("#")){
+                    System.out.println(i + " " + j);
+                }
+                }
+
+                //右端のマスの判定
+                if(j == W-1 && Array[i][H-2].equals("#")){
+                System.out.println(i + " " + (H-1));
+
+                }
+
             }
         }
-        System.out.println(X + " " + Y);
     }
 }
