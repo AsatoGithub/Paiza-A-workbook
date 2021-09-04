b3bf03cb09bd4b482a8c04222cec1bfad813e0b0
AsatoT <20195046@g.aut.ac.jp>
2021-09-04 21:17:38 +0900

    累積和の計算

--- Main.java
+++ Main.java
@@ -4,23 +4,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int H = sc.nextInt(); //行数
-        int W = sc.nextInt(); //列数
-        int Y = sc.nextInt(); //Y座標
-        int X = sc.nextInt(); //X座標
-        String[][] map = new String[H][W];
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        int answer = 0;
 
-        for(int i = 0; i<H; i++){
-            for(int j = 0; j<W; j++){
-                if(X == j && Y == i){
-                    System.out.print("!");
-                }else if(X == j || Y == i){
-                    System.out.print("*");
-                }else{
-                    System.out.print(".");
-                }
-            }
-            System.out.println();
+        for(int i = 0; i<N; i++){
+            list[i] = sc.nextInt();
+            answer = answer + list[i];
+            System.out.println(answer);
         }
+
     }
 }
