547bf9b27fda5e68ce5a005e62456e26d44adacd
AsatoT <20195046@g.aut.ac.jp>
2021-09-06 22:13:39 +0900

    区間和の計算

--- Main.java
+++ Main.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int answer = 0;
-
-        if(n % 3 == 1){
-            if(k % 3 == 0){
-                answer = -1;
-            }else{
-                answer = 0;
-            }
-        }else if(n % 3 == 2){
-            if(k % 3 == 0){
-                answer = -1;
-            }else{
-                answer = 0;
-            }
-            }else{
-                if(k % 3 == 0){
-                answer = -1;
-            }else{
-                answer = 0;
-            }
+        int N = sc.nextInt(); //要素数N
+        int[] Nlist = new int[N]; //要素格納配列
+        int answer = 0; //結果格納変数
+        for(int i = 0; i<N; i++){
+            Nlist[i] = sc.nextInt();
         }
-        System.out.println(answer);
+
+        int n = sc.nextInt(); //クエリの数
+        for(int i = 0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int j = a; j<=b; j++){
+                answer = answer + Nlist[j];
+            }
+            System.out.println(answer);
+            answer = 0; //answer初期化
+        }
+
     }
 }
