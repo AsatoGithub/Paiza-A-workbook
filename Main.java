9c7504ddd5383bff92e355d5c69116305cce21de
AsatoT <20195046@g.aut.ac.jp>
2021-09-08 00:49:34 +0900

    挿入ソート

--- Main.java
+++ Main.java
@@ -1,57 +1,40 @@
-import java.util.*;
+import java.util.Scanner;
 
+public class Main{
+	static void print(int a[], int n){
+		for(int i = 0; i < n; i++){
+			if(i > 0)	System.out.print(" ");
+			System.out.print(a[i]);
+		}
+		System.out.println();
+	}
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //要素数N
-        int[] Nlist = new int[N]; //要素格納配列
-        int answer = 0; //結果格納変数
-        for(int i = 0; i<N; i++){
-            Nlist[i] = sc.nextInt();
-        }
+	static void insertion_sort(int a[], int n){
+		for(int i = 1; i < n; i++){
+			int x = a[i];
+			int j = i-1;
 
-        int n = sc.nextInt(); //クエリの数
-        for(int i = 0; i<n; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            for(int j = a; j<=b; j++){
-                answer = answer + Nlist[j];
-            }
-            System.out.println(answer);
-            answer = 0; //answer初期化
-        }
+			while(j >= 0 && a[j] > x){
+				a[j+1] = a[j];
+				j--;
+			}
 
-    }
-}
-import java.util.*;
+			a[j+1] = x;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+			print(a, n);
+		}
+	}
 
-        if(n % 3 == 1){
-            if(k % 3 == 0){
-                System.out.println("0");
-            }else{
-                System.out.println("1");
-            }
-        }else if(n % 3 == 2){
-            if(k % 3 == 0){
-                System.out.println("-1");
-            }else{
-                System.out.println("0");
-            }
-        }else if(n % 3 == 0){
-            if(k % 3 == 0){
-                System.out.println("-1");
-            }else{
-                System.out.println("0");
-            }
-        }
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 
+		int n = sc.nextInt();
 
-    }
+		int a[] = new int[n];
+		for(int i = 0; i < n; i++){
+			a[i] = sc.nextInt();
+		}
+
+		insertion_sort(a, n);
+	}
 }
